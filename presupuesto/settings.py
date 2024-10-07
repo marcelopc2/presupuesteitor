b8b5dd416129addbb905dfa12233cc27b14196db
marcelopc2 <marcelopc2@gmail.com>
2024-10-06 23:13:33 -0300

    cambiando secret key

--- presupuesto/settings.py
+++ presupuesto/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', "Probando por mientras")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
